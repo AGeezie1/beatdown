097a22d2d1d5fd309a5f3b11fdbdf0295e0f6ba5
Asa Gee <asamingogee1@gmail.com>
2026-09-03 15:22:43 -0400

    the platform
    🌲

--- beatdown.py
+++ beatdown.py
@@ -16,6 +16,26 @@ clock = pygame.time.Clock()
 
 keys = pygame.key.get_pressed()
 
+
+
+class Platform:
+    def __init__(self,x,y,width,hight,color = (50,50,50)):
+        self.rect = pygame.Rect(x,y,width,hight)
+        self.color = color
+
+    def draw(self,surface):
+        pygame.draw.rect(surface,self.color,self.rect)
+        
+
+stage = Platform(100,350,600,150,(150,230,100))
+
+def perspective_shifter(direction, amount):
+    if direction == "left":
+        stage.rect.x -= amount
+    if direction == "right":
+        stage.rect.x += amount
+
+    
 running = True
 while running:
     # Handle events
@@ -24,30 +44,44 @@ while running:
             running = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                print("LEFT")
-            if event.key == pygame.K_UP:
-                print("UP")
-            if event.key == pygame.K_RIGHT:
-                print("RIGHT")
-            if event.key == pygame.K_DOWN:
-                print("DOWN")
-
-
+    
             if event.key == pygame.K_a:
-                print("LEFT")
+                print("1 LEFT")
             if event.key == pygame.K_w:
-                print("UP")
+                print("1 UP")
             if event.key == pygame.K_d:
-                print("RIGHT")
+                print("1 RIGHT")
             if event.key == pygame.K_s:
-                print("DOWN")
+                print("1 DOWN")
             
 
+            if event.key == pygame.K_LEFT:
+                print("2 LEFT")
+            if event.key == pygame.K_UP:
+                print("2 UP")
+            if event.key == pygame.K_RIGHT:
+                print("2 RIGHT")
+            if event.key == pygame.K_DOWN:
+                print("2 DOWN")
+
+
+
+            #just for some debugging 
+            if event.key == pygame.K_9:
+                perspective_shifter("left",5)
+            if event.key == pygame.K_0:
+                perspective_shifter("right",5)
+
+
+
             
 
+    
 
-    screen.fill((0, 0, 0)) 
+    screen.fill((240,240,240))
+    stage.draw(screen) 
+
+
     pygame.display.flip()
 
     clock.tick(FPS)
