6153937f21e6132266b4be1d9cd14486c0de072e
Asa Gee <asamingogee1@gmail.com>
2026-09-09 17:21:17 -0400

    smooooooooooooooooth operatoooor
    smooooth operator and music player

---
+++ music.py
@@ -0,0 +1,12 @@
+import pygame
+
+def start_music():
+    pygame.mixer.init()
+    pygame.mixer.music.load("smooth_operator.mp3")
+    pygame.mixer.music.set_volume(1.0)
+    pygame.mixer.music.play()
+
+start_music()
+
+while True:
+    pass
